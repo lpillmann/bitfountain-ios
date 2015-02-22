3fa92615e6a23976746723996fb6738c1df56ecf
Lui <lui.pillmann@gmail.com>
2015-02-23 00:43:25 +0100

    challange 1 completed

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var sendMessageButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -39,7 +40,19 @@ class ViewController: UIViewController {
 
         sendMessageButton.backgroundColor = color;
         sendMessageButton.setTitle("Message sent", forState: UIControlState.Normal);
+        
+        // lines for Challange 1
+        nameLabel.hidden = false;
+        nameLabel.text = enterNameTextField.text;
+        nameLabel.textColor = UIColor.blueColor();
+        enterNameTextField.text = "";
+        
     }
 
 }
+
+
+
+
+
 
